a09747a9b71cc4215d96a146c2102b4572d975cc
Zenen Contreras <zenencontreras1@gmail.com>
2026-05-21 23:51:36 -0500

    feat: Añadir funciones para manejar operaciones CRUD en el API de personas y logs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
+import { ToastProvider } from "./components/ui/Toast.jsx";
 import './index.css';
 
 function Auth0ProviderWithNavigate({ children }) {
@@ -31,7 +32,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Auth0ProviderWithNavigate>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </StrictMode>
